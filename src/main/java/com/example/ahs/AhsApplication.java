0a0c8b36fd019e4ffc9402c05cf4ae3315f9f768
Alexandra <alexandravolchihina@yandex.ru>
2023-04-29 16:54:12 +0300

    first

---
+++ src/main/java/com/example/ahs/AhsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ahs/AhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ahs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
